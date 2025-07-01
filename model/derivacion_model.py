408d002ff073d965146af58bea983cd932f7a363
Samuel <rene28609@gmail.com>
2025-06-30 22:17:35 -0500

    cree el archivo y el de guardar derivacion

---
+++ model/derivacion_model.py
@@ -0,0 +1,13 @@
+from database.config import get_connection
+from datetime import datetime
+
+def guardar_derivacion(numeros, desviacion):
+    conn = get_connection()
+    cursor = conn.cursor()
+    fecha = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    numeros_str = ','.join(map(str, numeros))
+    query = "INSERT INTO derivaciones (numeros, desviacion, fecha) VALUES (%s, %s, %s)"
+    cursor.execute(query, (numeros_str, desviacion, fecha))
+    conn.commit()
+    conn.close()
+
